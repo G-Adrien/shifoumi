a369d3806b6121ffbc93fbadcc009cabdce8c63d
Adrien Girin <girin.adrien.pro@gmail.com>
2020-07-02 16:01:18 +0200

    add: welcome message, user name, user choice

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//display a welcome message: 
+alert("Bonjour et bievenue" + "\n" + "sur le site du jeu du Shifoumi" );
+
+//ask a player name
+var userName = prompt("Veuillez insérer votre nom");
+
+
+var items=[
+    "Pierre",
+    "Feuille",
+    "Ciseaux"
+];
+
+
+// user choice betwen :"Pierre, Feuille, Ciseaux"
+
+var userChoice = prompt(userName + ", veuillez saisir ci-dessous entre : \"pierre, feuille, ciseaux \" ");
+
+console.log(userChoice)
+
+
+
+
+function itemChoice(){
+    prompt(userName + ", veuillez saisir ci-dessous entre : \"pierre, feuille, ciseaux \" ");
+    
+//  }
+
+// console.log(userChoice)
+
+// //compteur random choice
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * Math.floor(max));
+//   }
+//   console.log(getRandomInt(3));
+//   // expected output: 0, 1 or 2
+
+
+// function compterChoice(){
+
+// }
